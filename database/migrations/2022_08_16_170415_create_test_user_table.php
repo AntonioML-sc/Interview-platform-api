0520e05964976253c7d8cbb131a0802f8e9b4e13
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-08-16 19:17:22 +0200

    Feat(tests): create test_user table migration

---
+++ database/migrations/2022_08_16_170415_create_test_user_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_user', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('test_id')->nullable(false);
+            $table->foreign('test_id')
+                ->references('id')
+                ->on('tests')
+                ->onDelete('cascade');
+            $table->uuid('user_id')->nullable(false);
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->string('user_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_user');
+    }
+};
