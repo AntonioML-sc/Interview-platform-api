47494379e5a1577c8ff4ae2186d418ff3f5bd5fb
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-08-16 13:27:15 +0200

    Feat(users): Edit the Laravel default users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,20 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
+            $table->uuid('role_id')->nullable(false);
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+            $table->string('last_name');
+            $table->string('first_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone');
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
