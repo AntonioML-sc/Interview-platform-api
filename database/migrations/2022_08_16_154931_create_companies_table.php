62c331fddcfda586cfc97541200f2a9ec08596bf
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-08-18 13:13:53 +0200

    Refactor(companies): create column status in companies table to allow logical deletion

--- database/migrations/2022_08_16_154931_create_companies_table.php
+++ database/migrations/2022_08_16_154931_create_companies_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('address');
             $table->text('description');
+            $table->string('status');
             $table->timestamps();
         });
     }
